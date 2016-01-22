ead3a3b678e6f4e50afc948ad3713ab6266fe4d6
stellar910412 <stellarlele@163.com>
2016-01-22 18:27:30 +0800

    真实数据入库

--- application/models/Pvuv_device_model.php
+++ application/models/Pvuv_device_model.php
@@ -12,6 +12,7 @@ class Pvuv_device_model extends CI_Model {
         //$timeSelect = date("Y-m-d",strtotime("-3 day"));
         //return $timeSelect;
        // $deviceMacArray = array();
+        $timeFlag = date("Y-m-d",strtotime("-1 day"));
         $sqlForDeviceMac = "SELECT device_mac from `pvuv-log` group by device_mac";
         $deviceMacArray = $this->db->query($sqlForDeviceMac)->result();
         //return $deviceMacArray;
@@ -19,29 +20,28 @@ class Pvuv_device_model extends CI_Model {
         $deviceMacNum = count($deviceMacArray);
         for($i = 0 ;$i < $deviceMacNum;$i++)
         {
-            $sql = "SELECT count('{$deviceMacArray[$i]->device_mac}') FROM `pvuv-log` WHERE time BETWEEN '2016-01-15 00:00:00' AND '2016-01-15 23:59:59'";
+            $sql = "SELECT count('{$deviceMacArray[$i]->device_mac}') FROM `pvuv-log` WHERE time BETWEEN '{$timeFlag} 00:00:00' AND '{$timeFlag} 23:59:59'";
            // return $sql;
             $targetMac = $deviceMacArray[$i]->device_mac;
             //return $targetMac;
-            $pv = $this->db->query("SELECT * FROM `pvuv-log` WHERE time BETWEEN '2016-01-15 00:00:00' AND '2016-01-15 23:59:59' AND device_mac = '{$targetMac}'")->num_rows();
-            
+            $pv = $this->db->query("SELECT * FROM `pvuv-log` WHERE time BETWEEN '{$timeFlag} 00:00:00' AND '{$timeFlag} 23:59:59' AND device_mac = '{$targetMac}'")->num_rows();
             //14
             $download_app_times = $this->db->query("SELECT * FROM `pvuv-log` WHERE time BETWEEN '2016-01-15 00:00:00' AND '2016-1-15 23:59:59' AND device_mac = '{$targetMac}' AND url like '%download%'")->num_rows();
-            $uv = $this->db->query("SELECT remote_mac from `pvuv-log` WHERE time BETWEEN '2016-01-15 00:00:00' AND '2016-01-15 23:59:59' group by remote_mac")->num_rows();
+            $uv = $this->db->query("SELECT remote_mac from `pvuv-log` WHERE time BETWEEN '{$timeFlag} 00:00:00' AND '{$timeFlag} 23:59:59' AND device_mac = '{$targetMac}' group by remote_mac")->num_rows();
             //return $uv;
             //11
-            $uv_android = $this->db->query("SELECT remote_mac from `pvuv-log` WHERE time BETWEEN '2016-01-15 00:00:00' AND '2016-01-15 23:59:59' AND http_user_agent='android' group by remote_mac")->num_rows();
+            $uv_android = $this->db->query("SELECT remote_mac from `pvuv-log` WHERE time BETWEEN '{$timeFlag} 00:00:00' AND '{$timeFlag} 23:59:59' AND http_user_agent like '%android%' AND device_mac = '{$targetMac}' group by remote_mac")->num_rows();
             //return $uv_android_12;
             //12
-            $uv_unknow = $this->db->query("SELECT remote_mac from `pvuv-log` WHERE time BETWEEN '2016-01-15 00:00:00' AND '2016-01-15 23:59:59' AND http_user_agent='unknow' group by remote_mac")->num_rows();
+            $uv_unknow = $this->db->query("SELECT remote_mac from `pvuv-log` WHERE time BETWEEN '{$timeFlag} 00:00:00' AND '{$timeFlag} 23:59:59' AND http_user_agent like'%unknow%' AND device_mac = '{$targetMac}' group by remote_mac")->num_rows();
             //return $uv_unknow_12;
             //1
-            $uv_ios = $this->db->query("SELECT remote_mac from `pvuv-log` WHERE time BETWEEN '2016-01-15 00:00:00' AND '2016-01-15 23:59:59' AND http_user_agent = 'ios' group by remote_mac")->num_rows();
+            $uv_ios = $this->db->query("SELECT remote_mac from `pvuv-log` WHERE time BETWEEN '{$timeFlag} 00:00:00' AND '{$timeFlag} 23:59:59' AND http_user_agent like '%ios%' AND device_mac = '{$targetMac}' group by remote_mac")->num_rows();
             
             //2
-            $uv_windows = $this->db->query("SELECT remote_mac from `pvuv-log` WHERE time BETWEEN '2016-01-15 00:00:00' AND '2016-01-15 23:59:59' AND http_user_agent = 'windows' group by remote_mac")->num_rows();
+            $uv_windows = $this->db->query("SELECT remote_mac from `pvuv-log` WHERE time BETWEEN '{$timeFlag} 00:00:00' AND '{$timeFlag} 23:59:59' AND http_user_agent like '%windows%' AND device_mac = '{$targetMac}' group by remote_mac")->num_rows();
             //return $uv_windows;
-            $query = $this->db->query("INSERT INTO `pvuv-device` (device_mac,time,pv,download_app_times,uv,uv_android,uv_ios,uv_windows,uv_others) VALUES ('{$targetMac}','2016-01-15','{$pv}','{$download_app_times}','{$uv}','{$uv_android}','{$uv_ios}','{$uv_windows}','{$uv_unknow}')");
+            $query = $this->db->query("INSERT INTO `pvuv-device` (device_mac,time,pv,download_app_times,uv,uv_android,uv_ios,uv_windows,uv_others) VALUES ('{$targetMac}','{$timeFlag}','{$pv}','{$download_app_times}','{$uv}','{$uv_android}','{$uv_ios}','{$uv_windows}','{$uv_unknow}')");
             
         }    
 //        $device_mac_array = $this->db->query("SELECT device_mac from `pvuv-log` WHERE time like '%{$time_latest}%'")->num_rows();
--- application/models/Pvuv_log_model.php
+++ application/models/Pvuv_log_model.php
@@ -30,49 +30,53 @@ class Pvuv_log_model extends CI_Model {
 //                {
 //                    continue;
 //                }
-                $file_contents_array = file($dirFile);
-                $per_num = 0;
+//                else
+//                {
                 
-                while(!empty($file_contents_array[$per_num]))
-                {
+                    $file_contents_array = file($dirFile);
+                    $per_num = 0;
+                
+                    while(!empty($file_contents_array[$per_num]))
+                    {
                     
-                    $file_content_per_array = json_decode($file_contents_array[$per_num],true);
+                        $file_content_per_array = json_decode($file_contents_array[$per_num],true);
 //                    if(property_exists($file_content_per_array,"remote_addr") && property_exists($file_content_per_array,"remote_mac") && property_exists($file_content_per_array,"time_iso8601") && property_exists($file_content_per_array,"request_method") && property_exists($file_content_per_array,"uri") && property_exists($file_content_per_array,"server_protocol") && property_exists($file_content_per_array,"status") && property_exists($file_content_per_array,"body_bytes_sent") && property_exists($file_content_per_array,"http_user_agent") && property_exists($file_content_per_array,"host") && property_exists($file_content_per_array,"request_filename"))
 //                    {
                         
                         
-                        $remote_ip = $file_content_per_array['remote_addr'];
-                        $remote_mac = $file_content_per_array['remote_mac'];
-                        $time_content = $file_content_per_array['time_iso8601'];
-                        $timeYmd = substr($time_content,0,10);
-                        $timeHms = substr($time_content,-14,8);
-                        $time = $timeYmd.' '.$timeHms;
-                        $time_local = substr($time_content,-5,2).substr($time_content,-2,2);
-                        $request = $file_content_per_array['request_method'];
-                        $url = $file_content_per_array['uri'];
-                        $protocol = $file_content_per_array['server_protocol'];
-                        $status = $file_content_per_array['status'];
-                        $body_bytes_sent = $file_content_per_array['body_bytes_sent'];
-                        $http_user_agent = $file_content_per_array['http_user_agent'];
-                        $host = $file_content_per_array['host'];
+                            $remote_ip = $file_content_per_array['remote_addr'];
+                            $remote_mac = $file_content_per_array['remote_mac'];
+                            $time_content = $file_content_per_array['time_iso8601'];
+                            $timeYmd = substr($time_content,0,10);
+                            $timeHms = substr($time_content,-14,8);
+                            $time = $timeYmd.' '.$timeHms;
+                            $time_local = substr($time_content,-5,2).substr($time_content,-2,2);
+                            $request = $file_content_per_array['request_method'];
+                            $url = $file_content_per_array['uri'];
+                            $protocol = $file_content_per_array['server_protocol'];
+                            $status = $file_content_per_array['status'];
+                            $body_bytes_sent = $file_content_per_array['body_bytes_sent'];
+                            $http_user_agent = $file_content_per_array['http_user_agent'];
+                            $host = $file_content_per_array['host'];
                         //if($this->db->query("SELECT * from `pvuv-log` WHERE device_mac = $device_mac AND time like '%{$}%'"))
                         //判断需要加入规则匹配request_filename
-                        if(isset($remote_mac) && $file_content_per_array['host'] === '192.168.0.1')
-                        {
-                            $sql = "INSERT INTO `pvuv-log` (device_mac,remote_ip,remote_mac,time,time_local,request,url,protocol,status,body_bytes_sent,http_user_agent) values ('{$device_mac}','{$remote_ip}','{$remote_mac}','{$time}','{$time_local}','{$request}','{$url}','{$protocol}','{$status}','{$body_bytes_sent}','{$http_user_agent}')";
+                            if(isset($remote_mac) && $file_content_per_array['host'] === '192.168.0.1')
+                            {
+                                $sql = "INSERT INTO `pvuv-log` (device_mac,remote_ip,remote_mac,time,time_local,request,url,protocol,status,body_bytes_sent,http_user_agent) values ('{$device_mac}','{$remote_ip}','{$remote_mac}','{$time}','{$time_local}','{$request}','{$url}','{$protocol}','{$status}','{$body_bytes_sent}','{$http_user_agent}')";
                         
                             
-                        }
+                            }
 //                    }
-                    if(!$this->db->query($sql))
-                    {
-                        $flag = 1;
+                        if(!$this->db->query($sql))
+                        {
+                            $flag = 1;
+                        }
+                        $per_num += 1;                    
                     }
-                    $per_num += 1;                    
-                }
+//                }
                 //fclose($dirFile);
+                }
             }
-        }
         //closedir($dirname);
         return $flag;
     }
