9c23b6ff15bf8fe26d0d3970ddf059f92a6de5e1
wangmingguang <wangmingguang>
2016-03-15 09:28:21 +0800

    scanAp

---
+++ application/models/ScanAP_model.php
@@ -0,0 +1,25 @@
+<?php
+class ScanAP_model extends CI_Model{
+    public function __construct(){
+        parent::__construct();
+        $this->load->database();
+    }
+    
+    public function info_nums(){
+       return $this->db->count_all('scanApList'); 
+    }
+    
+    public function register($array){
+        $sql = "insert into scanApList values({$array['ap_mac']}, {$array['wlan_src']},
+                {$array['wlan_rssi']}, {$array['wlan_essid']}, {$array['wlan_mode']}, {$array['wlan_channel']},
+                {$array['wlan_encrypt']})";
+        
+        return $this->db->query($sql);
+    }
+    
+    public function ap_info($offset, $pagesize){
+        $sql = "select * from `scanApList` limit $offset,$pagesize";
+        
+        return $this->db->query($sql)->result_array();
+    }
+}
